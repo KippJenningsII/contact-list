c53e1ecfcffc8b85b885c63b2cb2854d0161519b
kippo <jenningskipp@gmail.com>
2020-11-22 17:25:22 -0600

    changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,50 @@
-import React from'react';
+import React, {
+    useState
+} from'react';
 import ReactDOM from 'react-dom';
+
+import './style.css';
+
 
 
 
 const App = () => {
+    const [info, displayName] = useState();
+
+
     return(
         <div id="App">
-            <form> 
-                <ul>
-                <li><label>
-                    Contact Name:
-                    <input type="text" name="name" />
-                </label></li>
-                <li><label>
-                    Address:
-                    <input type="text" name="name" />
-                </label></li>
-                <label>
-                <input type="submit" value="Submit" />
-                </label>
-                </ul>
+            <h2>Contact List</h2>
+            <h3>Add New Contact</h3>
+            <form class="contact-input"> 
+                <label>Contact Name </label>
+                <input type="text" name="name" />
+                <br />
+                <label>Address </label>
+                <input type="text" name="address" />
+                <br />
+                <label> Email </label>
+                <input type="text" name="email" />
+                <br />
+                <label> Phone Number </label>
+                <input type="text" name="phone number" />
+                <br />
+                <label>Contact Type</label>
+                <select>
+                    <option value="work">work</option>
+                    <option value="personal">personal</option>
+                    <option value="other">other</option>
+                </select>
+                <br />
+                <button>Submit</button>
             </form>
         </div>
+
     )
 }
+
+
+
 
 
 
