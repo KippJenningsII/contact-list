8d72cfa5abf7ca63b78be0d8d170fd84600e0baa
kippo <jenningskipp@gmail.com>
2020-11-22 18:07:29 -0600

    updated

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import './style.css';
 
 
 const App = () => {
-    const [info, displayName] = useState();
 
 
     return(
@@ -63,21 +62,3 @@ ReactDOM.render(
 
 
 
-async function fetchAPI(url, method="GET", sendData=null) {
-    const fetchOptions = {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Implbm5pbmdza2lwcCIsImlhdCI6MTYwNTc0NjE3NywiZXhwIjoxNjA2MzUwOTc3fQ.Spr-OmuBZt-N0gqgo0nGMm9T21P65E2N-h3oQ7Gc_kk'
-      }
-    };
-  
-    if (sendData) {
-      fetchOptions.body = JSON.stringify(sendData);
-    }
-  
-    const response = await fetch(url, fetchOptions);
-    const data = await response.json();
-  
-    return data;
-  }
