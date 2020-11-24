3b6b15a4a869ee32c912ccd3cdecaddfb9616efe
kippo <jenningskipp@gmail.com>
2020-11-23 19:19:27 -0600

    build folder

---
+++ build/precache-manifest.c85461ef3f071625e0d699975f5933ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce3af8c81342a84f9269660556d4592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c459c61b210c83dca534",
+    "url": "/static/css/main.ad12b1cd.chunk.css"
+  },
+  {
+    "revision": "f3a8b85f5e748d55dc28",
+    "url": "/static/js/2.61d7bd68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.61d7bd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c459c61b210c83dca534",
+    "url": "/static/js/main.3900f4c7.chunk.js"
+  },
+  {
+    "revision": "f3f8fb5c825638ea9727",
+    "url": "/static/js/runtime-main.e93c7fde.js"
+  }
+]);
